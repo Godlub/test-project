6b913110ec53c638de9ba2c430d7150135c2b36e
Gleb Shiryaev <glebwar33@gmail.com>
2020-12-31 22:45:03 +0300

    Init

--- Main.cpp
+++ Main.cpp
@@ -44,23 +44,21 @@ int main() {
             cout << endl;
             x0 += deltaX;
         }
-    } else if (x0 < x1) {
-        if (deltaX <= 0) {
-            cout << "Error";
-            return 0;
+    } else if (deltaX <= 0) {
+        cout << "Error";
+        return 0;
+    } else
+        cout << "|" << setw(13) << "line number" << " | " << setw(14) << "x= | " << setw(14) << "value |" << endl;
+    while (x0 <= x1) {
+        cout << "|" << setw(13) << n << " | ";
+        n++;
+        cout << setw(11) << x0 << " | ";
+        if (isnan(fun(x0))) {
+            cout << setw(14) << "Error |";
         } else
-            cout << "|" << setw(13) << "line number" << " | " << setw(14) << "x= | " << setw(14) << "value |" << endl;
-        while (x0 <= x1) {
-            cout << "|" << setw(13) << n << " | ";
-            n++;
-            cout << setw(11) << x0 << " | ";
-            if (isnan(fun(x0))) {
-                cout << setw(14) << "Error |";
-            } else
-                cout << setw(12) << fun(x0) << " |";
-            cout << endl;
-            x0 += deltaX;
-        }
+            cout << setw(12) << fun(x0) << " |";
+        cout << endl;
+        x0 += deltaX;
     }
     return 0;
 }
